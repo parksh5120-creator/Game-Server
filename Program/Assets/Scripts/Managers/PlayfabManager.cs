95bf38853867557a0a294e520584ff7d182bbd4a
parksh5120-creator <parksh5120@gmail.com>
2026-06-13 21:38:46 +0900

    (2026.6.12) 로그인 시스템

--- Program/Assets/Scripts/Managers/PlayfabManager.cs
+++ Program/Assets/Scripts/Managers/PlayfabManager.cs
@@ -1,12 +1,65 @@
 using UnityEngine;
 using Photon.Pun;
+using PlayFab;
 using PlayFab.ClientModels;
 using TMPro;
+using System.Collections;
 
-public class PlayfabManager : MonoBehaviour
+public class PlayfabManager : MonoBehaviourPunCallbacks
 {
     [SerializeField] string version;
 
     [SerializeField] TMP_InputField addressInputField;
     [SerializeField] TMP_InputField passwordInputField;
+
+    public void Request()
+    {
+        var request = new LoginWithEmailAddressRequest 
+        {
+            Email = addressInputField.text,
+            Password = passwordInputField.text
+        };
+
+        PlayFabClientAPI.LoginWithEmailAddress(request, Success, Failed);
+    }
+
+    public void Success(LoginResult loginResult)
+    {
+        PlayFabClientAPI.GetAccountInfo(new GetAccountInfoRequest(), Success, Failed);
+
+        PhotonNetwork.AutomaticallySyncScene = false;
+
+        PhotonNetwork.GameVersion = version;
+
+        StartCoroutine(ConnectRoutine());
+    }
+
+    public void Success(GetAccountInfoResult getAccountInfoResult)
+    {
+        PhotonNetwork.LocalPlayer.NickName = getAccountInfoResult.AccountInfo?.Username;
+    }
+
+    public void Failed(PlayFabError playFabError)
+    {
+        Debug.Log(playFabError.GenerateErrorReport());
+    }
+
+    private IEnumerator ConnectRoutine()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+
+        while(PhotonNetwork.IsConnectedAndReady == false)
+        {
+            yield return null;
+        }
+
+        PhotonNetwork.JoinLobby();
+
+        Debug.Log("Connected to Photon");
+    }
+
+    public override void OnJoinedLobby()
+    {
+        PhotonNetwork.LoadLevel("Lobby");
+    }
 }
