cfbfae6f645f1ed7238f433392d55929506f2fe3
parksh5120-creator <parksh5120@gmail.com>
2026-06-23 21:43:42 +0900

    (2026.6.23) 룸 설정 시스템

--- Program/Assets/Scripts/User Interfaces/RoomPanel.cs
+++ Program/Assets/Scripts/User Interfaces/RoomPanel.cs
@@ -3,11 +3,51 @@ using UnityEngine;
 using Photon.Realtime;
 using TMPro;
 using System;
+using UnityEngine.UI;
 
 public class RoomPanel : MonoBehaviourPunCallbacks
 {
+    [SerializeField] int personnel = 0;
+    [SerializeField] Toggle [] toggles;
+    [SerializeField] Button createRoomButton;
+    [SerializeField] TMP_InputField roomNameInputField;
+
+    private void Start()
+    {
+        Select();
+
+        OnRoomNameChanged();
+    }
     public void CreateRoom()
     {
         RoomOptions roomOptions = new RoomOptions();
+
+        roomOptions.MaxPlayers = personnel;
+
+        roomOptions.IsOpen = true;
+
+        roomOptions.IsVisible = true;
+
+        PhotonNetwork.CreateRoom(roomNameInputField.text, roomOptions);
+
+        gameObject.SetActive(false);
+    }
+
+    public void OnRoomNameChanged()
+    {
+        createRoomButton.interactable = !string.IsNullOrWhiteSpace(roomNameInputField.text) == false;
+    }
+
+    public void Select()
+    {
+        for (int i = 0; i < toggles.Length; i++)
+        {
+            if (toggles[i].isOn)
+            {
+                personnel = i + 2;
+                Debug.Log($"인원 수 = {personnel}");
+                break;
+            }
+        }
     }
 }
